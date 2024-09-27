225eb43c93471824f3fd848ac6c8246bf8204d4c
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-26 19:15:05 -0600

    open firewall

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -5,6 +5,9 @@ mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 
+# Open firewall
+sudo ufw allow 443
+
 # Install certbot
 sudo apt-get install certbot
 certbot certonly --standalone -d patterned-landscapes.stephentcasey.com --email thornhill52320@gmail.com -n --agree-tos --no-eff-email
