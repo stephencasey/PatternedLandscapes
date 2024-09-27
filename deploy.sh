8e73e9c1f9e2fdea750e46b6e383d85d0098844f
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-26 21:16:54 -0600

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ cd /home/thornhill523/patterned-landscapes
 git pull
 docker stop $(docker ps -a -q)
 docker system prune -a -f
-docker compose -f docker-compose.initial.yml up --build
+docker compose up --build
