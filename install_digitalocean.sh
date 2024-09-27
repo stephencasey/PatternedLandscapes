ceee3e0a510ff6e91acef4c828fe723610fe0e38
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-26 21:49:12 -0600

    switch to certbot docker

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -10,6 +10,15 @@ sudo ufw allow 80
 sudo ufw allow 443
 
 # Install certbot
-docker compose -f docker-compose.initial.yml up --build
-docker compose up --build
+docker-compose -f ./docker-compose-initiate.yaml up -d nginx
+docker-compose -f ./docker-compose-initiate.yaml up certbot
+docker-compose -f ./docker-compose-initiate.yaml down
+
+# some configurations for let's encrypt
+curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
+
+# Phase 2
+crontab ./etc/crontab
+docker-compose -f ./docker-compose.yaml up
 
