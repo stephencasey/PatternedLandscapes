d5cdbe211789dd3a7b112a9a62d079e876d39c70
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-26 21:51:46 -0600

    docker space compose

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -10,9 +10,9 @@ sudo ufw allow 80
 sudo ufw allow 443
 
 # Install certbot
-docker-compose -f ./docker-compose-initiate.yaml up -d nginx
-docker-compose -f ./docker-compose-initiate.yaml up certbot
-docker-compose -f ./docker-compose-initiate.yaml down
+docker compose -f ./docker-compose-initiate.yaml up -d nginx
+docker compose -f ./docker-compose-initiate.yaml up certbot
+docker compose -f ./docker-compose-initiate.yaml down
 
 # some configurations for let's encrypt
 curl -L --create-dirs -o etc/letsencrypt/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
@@ -20,5 +20,5 @@ openssl dhparam -out etc/letsencrypt/ssl-dhparams.pem 2048
 
 # Phase 2
 crontab ./etc/crontab
-docker-compose -f ./docker-compose.yaml up
+docker compose -f ./docker-compose.yaml up
 
