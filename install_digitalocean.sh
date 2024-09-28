2b3020f2c771514c222af987dd757ae307c09195
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-27 21:07:27 -0600

    add docker install to shell script

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install docker
+sudo apt update
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+sudo apt install docker-ce
+sudo usermod -aG docker ${USER}
+sudo su - ${USER}
+
 # Install docker compose
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
