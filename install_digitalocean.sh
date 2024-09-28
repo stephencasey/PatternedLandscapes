ea9e23ff4082246a638148ead4c27c0ccb8c2b25
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-27 21:14:26 -0600

    root

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -6,8 +6,8 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
 sudo apt install docker-ce -y
-sudo usermod -aG docker ${USER}
-sudo su - ${USER}
+sudo usermod -aG docker thornhill523
+sudo su - thornhill523
 
 # Install docker compose
 mkdir -p ~/.docker/cli-plugins/
