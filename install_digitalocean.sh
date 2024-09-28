8742c14c8ff1cdc64f3c980efb87b206ac741e5c
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-27 21:17:25 -0600

    remove docker install from script

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Install docker
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-sudo apt install docker-ce -y
-sudo usermod -aG docker thornhill523
-sudo su - thornhill523
-cd patternedlandscapes
-
 # Install docker compose
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
