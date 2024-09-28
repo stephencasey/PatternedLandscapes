e88f789e97dfb7c71ad6ad3e48674d8ac06a211c
Stephen Casey <stephen.thornhill.casey@gmail.com>
2024-09-27 21:09:10 -0600

    dash y

--- install_digitalocean.sh
+++ install_digitalocean.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 sudo su - ${USER}
 
