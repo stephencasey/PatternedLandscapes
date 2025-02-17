e553f5b2f6bce64b4da0b8ca9173b9a1f31242d7
Stephen Casey <stephen.thornhill.casey@gmail.com>
2025-02-17 10:52:18 -0700

    move out all nginx and certbot

--- certbot-initial/certbot-initial-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-certbot certonly \
-        --standalone \
-        -d landscape-patterns.stephentcasey.com\
-        -m thornhill52320@gmail.com \
-        --rsa-key-size "2048" \
-        --agree-tos \
-        -n
-        # --force-renewal
-        # -vvv
--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/thornhill523/patterned-landscapes
-git pull
-docker stop $(docker ps -a -q)
-docker system prune -a -f
-docker compose up --build
